4069a8b10c396cefc3c4a7bb7ab614b3c6fd9c96
GustavoYamauchi <gyxgames31@gmail.com>
2021-05-30 03:14:07 -0300

    Criando a tela de carrinho e coloquei os icones de cart nas telas

--- SoBala/app/src/main/java/com/felipenishino/sobala/activities/ListProductsActivity.kt
+++ SoBala/app/src/main/java/com/felipenishino/sobala/activities/ListProductsActivity.kt
@@ -4,6 +4,9 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import com.felipenishino.sobala.R
 import com.felipenishino.sobala.databinding.ActivityListProductsBinding
 import com.felipenishino.sobala.databinding.ProductCardBinding
 import com.felipenishino.sobala.db.ProdutoService
@@ -30,7 +33,20 @@ class ListProductsActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+      when(item.itemId) {
+            R.id.cart -> {
+                val i = Intent(this, CartActivity::class.java)
+                startActivity(i)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
 
     fun updateUI(products: List<Product>) {
@@ -55,8 +71,6 @@ class ListProductsActivity : AppCompatActivity() {
     }
 
     fun refreshProducts() {
-
-
         listAllProducts()
     }
 
